7138ae1e09b5b717c6d34eedf43298dde8e0f278
Ben Bankes <ben@playgroundsessions.com>
2021-07-24 08:41:48 -0600

    Create the response from CSV data

--- app/Http/Controllers/StudentProgressController.php
+++ app/Http/Controllers/StudentProgressController.php
@@ -1,85 +1,238 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\Lesson;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\JsonResponse;
 
 class StudentProgressController extends Controller
 {
     public function get(int $userId): JsonResponse
     {
-        /** @var Lesson[] $lessons */
-        $lessons = Lesson::with(['segments','segments.practiceRecords' => function(HasMany $query) use ($userId) {
-            $query->where('user_id', $userId);
-        }])
-            ->where('is_published',true)
-            ->get();
-
-        $csv = new CsvFile();
-
-        $firstLesson = $lessons[0];
-        $lessonHeader = array_keys($firstLesson->attributesToArray());
-        $firstSegment = $firstLesson->segments[0];
-        $segmentHeader = array_keys($firstSegment->attributesToArray());
-        $firstPracticeRecord = $firstSegment->practiceRecords[0];
-        $practiceRecordHeader = array_keys($firstPracticeRecord->attributesToArray());
-
-        $header = [];
-
-        foreach ($lessonHeader as $headerValue) {
-            $header[] = 'lesson_' . $headerValue;
-        }
-
-        foreach ($segmentHeader as $headerValue) {
-            $header[] = 'segment_' . $headerValue;
-        }
-
-        foreach ($practiceRecordHeader as $headerValue) {
-            $header[] = 'practice_record_' . $headerValue;
-        }
-
-        $csv->addRow($header);
-
-        foreach ($lessons as $lesson) {
-            foreach ($lesson->segments as $segment) {
-                foreach ($segment->practiceRecords as $practiceRecord) {
-                    $lessonValues = array_values($lesson->attributesToArray());
-                    $segmentValues = array_values($segment->attributesToArray());
-                    $practiceRecordValues = array_values($practiceRecord->attributesToArray());
-                    $row = array_merge($lessonValues, $segmentValues, $practiceRecordValues);
-                    $csv->addRow($row);
-                }
-            }
-        }
-
-        $csv->export('data.csv');
-        exit;
+        return response()->json((array)(new LessonCsv())->getAllLessons($userId));
     }
 }
 
-class CsvFile
+class LessonCsv
 {
+    const LARAVEL_DEFAULT_DATE_FORMAT = 'Y-m-d\TH:i:s\.uT';
+
     private $fp;
 
     public function __construct()
     {
-        $this->fp = tmpfile();
+        $this->fp = fopen('../data.csv', 'r');
+        $this->discardRow(); // The header has column names, not data
     }
 
-    public function addRow(array $columns = []): void
+    private function discardRow(): void
     {
-        fputcsv($this->fp, $columns);
+        fgetcsv($this->fp);
     }
 
-    public function export($filename)
+    private function getRow(): ?LessonCsvRow
     {
-        header('Content-type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $filename . '"');
+        $row = fgetcsv($this->fp);
 
-        fseek($this->fp, 0);
-        fpassthru($this->fp);
-        exit;
+        if ($row === false) {
+            return null;
+        }
+
+        return new LessonCsvRow(
+            (int)$row[0],
+            (string)$row[1],
+            (string)$row[2],
+            (int)$row[3],
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[4]),
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[5]),
+            (bool)$row[6],
+            (int)$row[7],
+            (int)$row[8],
+            (string)$row[9],
+            (int)$row[10],
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[11]),
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[12]),
+            (int)$row[13],
+            (int)$row[14],
+            (int)$row[15],
+            (string)$row[16],
+            (float)$row[17],
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[18]),
+            \DateTime::createFromFormat(self::LARAVEL_DEFAULT_DATE_FORMAT, (string)$row[19]),
+            (int)$row[20],
+        );
     }
+
+    /** @return LessonResponse[] */
+    public function getAllLessons(int $userId): array
+    {
+        $lessons = [];
+        $currentLessonId = 0;
+        $currentSegmentId = 0;
+        $currentRow = $this->getRow();
+        $practiceRecordsForCurrentSegment = [];
+        $segmentsForCurrentLesson = [];
+
+        while ($nextRow = $this->getRow()) {
+            $isForCurrentUser = $nextRow->practiceRecordUserId === $userId;
+
+            if (!$isForCurrentUser) {
+                continue;
+            }
+
+            $practiceRecordsForCurrentSegment[] = self::getPracticeRecordFromRow($currentRow);
+
+            $isNewSegment = $nextRow->segmentId !== $currentSegmentId;
+
+            if ($isNewSegment) {
+                $segmentsForCurrentLesson[] = self::getSegmentFromRow($currentRow, $practiceRecordsForCurrentSegment);
+                $practiceRecordsForCurrentSegment = [];
+            }
+
+            $isNewLesson = $nextRow->lessonId !== $currentLessonId;
+
+            if ($isNewLesson) {
+                $lessons[] = self::getLessonResponseFromRow($currentRow, $segmentsForCurrentLesson);
+                $segmentsForCurrentLesson = [];
+            }
+
+            $currentRow = $nextRow;
+        }
+
+        return $lessons;
+    }
+
+    private static function getLessonResponseFromRow(LessonCsvRow $row, array $segments): LessonResponse
+    {
+        return new LessonResponse(
+            $row->lessonId,
+            $row->lessonName,
+            $row->lessonDescription,
+            $row->lessonDifficulty,
+            $row->lessonCreatedAt,
+            $row->lessonUpdatedAt,
+            $row->isLessonPublished,
+            $segments,
+        );
+    }
+
+    private static function getSegmentFromRow(LessonCsvRow $row, array $practiceRecords): Segment
+    {
+        return new Segment(
+            $row->segmentId,
+            $row->segmentLessonId,
+            $row->segmentName,
+            $row->segmentOrder,
+            $row->segmentCreatedAt,
+            $row->segmentUpdatedAt,
+            $practiceRecords,
+        );
+    }
+
+    private static function getPracticeRecordFromRow(LessonCsvRow $row): PracticeRecord
+    {
+        return new PracticeRecord(
+            $row->practiceRecordId,
+            $row->practiceRecordSegmentId,
+            $row->practiceRecordUserId,
+            $row->practiceRecordSessionUuid,
+            $row->practiceRecordTempoMultiplier,
+            $row->practiceRecordCreatedAt,
+            $row->practiceRecordUpdatedAt,
+            $row->practiceRecordScore,
+        );
+    }
+}
+
+class LessonCsvRow
+{
+    public function __construct(
+        public int $lessonId,
+        public string $lessonName,
+        public string $lessonDescription,
+        public int $lessonDifficulty,
+        public \DateTime $lessonCreatedAt,
+        public \DateTime $lessonUpdatedAt,
+        public bool $isLessonPublished,
+        public int $segmentId,
+        public int $segmentLessonId,
+        public string $segmentName,
+        public int $segmentOrder,
+        public \DateTime $segmentCreatedAt,
+        public \DateTime $segmentUpdatedAt,
+        public int $practiceRecordId,
+        public int $practiceRecordSegmentId,
+        public int $practiceRecordUserId,
+        public string $practiceRecordSessionUuid,
+        public float $practiceRecordTempoMultiplier,
+        public \DateTime $practiceRecordCreatedAt,
+        public \DateTime $practiceRecordUpdatedAt,
+        public int $practiceRecordScore,
+    ) {}
+}
+
+class LessonResponse implements \IteratorAggregate
+{
+    public array $segments;
+
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $description,
+        public int $difficulty,
+        public \DateTime $createdAt,
+        public \DateTime $updatedAt,
+        public bool $isPublished,
+        array $segments,
+    ) {
+        foreach ($segments as $segment) {
+            $this->addSegment($segment);
+        }
+    }
+
+    private function addSegment(Segment $segment): void
+    {
+        $this->segments[] = $segment;
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this);
+    }
+}
+
+class Segment
+{
+    public function __construct(
+        public int $id,
+        public int $lessonId,
+        public string $name,
+        public int $order,
+        public \DateTime $createdAt,
+        public \DateTime $updatedAt,
+        public array $practiceRecords,
+    ) {
+        foreach ($practiceRecords as $practiceRecord) {
+            $this->addPracticeRecord($practiceRecord);
+        }
+    }
+
+    private function addPracticeRecord(PracticeRecord $practiceRecord): void
+    {
+        $this->practiceRecords[] = $practiceRecord;
+    }
+}
+
+class PracticeRecord
+{
+    public function __construct(
+        public int $id,
+        public int $segmentId,
+        public int $userId,
+        public string $sessionUuid,
+        public float $tempoMultiplier,
+        public \DateTime $createdAt,
+        public \DateTime $updatedAt,
+        public int $score,
+    ) {}
 }
