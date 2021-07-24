dc8829338be7fa2d7beb574573d70cdb7d03acd3
Ben Bankes <ben@playgroundsessions.com>
2021-07-24 13:27:19 -0600

    Remove unnecessary docblocks

--- app/ViewModels/LessonCsv/LessonCsvRow.php
+++ app/ViewModels/LessonCsv/LessonCsvRow.php
@@ -33,169 +33,106 @@ class LessonCsvRow implements LessonCsvRowInterface
     {
     }
 
-    /**
-     * @return int
-     */
     public function getLessonId(): int
     {
         return $this->lessonId;
     }
 
-    /**
-     * @return string
-     */
     public function getLessonName(): string
     {
         return $this->lessonName;
     }
 
-    /**
-     * @return string
-     */
     public function getLessonDescription(): string
     {
         return $this->lessonDescription;
     }
 
-    /**
-     * @return int
-     */
     public function getLessonDifficulty(): int
     {
         return $this->lessonDifficulty;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getLessonCreatedAt(): \DateTime
     {
         return $this->lessonCreatedAt;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getLessonUpdatedAt(): \DateTime
     {
         return $this->lessonUpdatedAt;
     }
 
-    /**
-     * @return bool
-     */
     public function isLessonPublished(): bool
     {
         return $this->isLessonPublished;
     }
 
-    /**
-     * @return int
-     */
     public function getSegmentId(): int
     {
         return $this->segmentId;
     }
 
-    /**
-     * @return int
-     */
     public function getSegmentLessonId(): int
     {
         return $this->segmentLessonId;
     }
 
-    /**
-     * @return string
-     */
     public function getSegmentName(): string
     {
         return $this->segmentName;
     }
 
-    /**
-     * @return int
-     */
     public function getSegmentOrder(): int
     {
         return $this->segmentOrder;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getSegmentCreatedAt(): \DateTime
     {
         return $this->segmentCreatedAt;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getSegmentUpdatedAt(): \DateTime
     {
         return $this->segmentUpdatedAt;
     }
 
-    /**
-     * @return int
-     */
     public function getPracticeRecordId(): int
     {
         return $this->practiceRecordId;
     }
 
-    /**
-     * @return int
-     */
     public function getPracticeRecordSegmentId(): int
     {
         return $this->practiceRecordSegmentId;
     }
 
-    /**
-     * @return int
-     */
     public function getPracticeRecordUserId(): int
     {
         return $this->practiceRecordUserId;
     }
 
-    /**
-     * @return string
-     */
     public function getPracticeRecordSessionUuid(): string
     {
         return $this->practiceRecordSessionUuid;
     }
 
-    /**
-     * @return float
-     */
     public function getPracticeRecordTempoMultiplier(): float
     {
         return $this->practiceRecordTempoMultiplier;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getPracticeRecordCreatedAt(): \DateTime
     {
         return $this->practiceRecordCreatedAt;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getPracticeRecordUpdatedAt(): \DateTime
     {
         return $this->practiceRecordUpdatedAt;
     }
 
-    /**
-     * @return int
-     */
     public function getPracticeRecordScore(): int
     {
         return $this->practiceRecordScore;
